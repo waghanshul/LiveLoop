9e7af0a0e8605b83760ace8638f118c101d6ee15
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-11-17 18:54:36 +0530

    client side changes

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -2,16 +2,17 @@
 
 const { Socket } = require('socket.io');
 
-const io = require('socket.io')(8000)
+const io = require('socket.io')(8080)
 
 const users = {};
 
 io.on('connection', socket =>{
     socket.on('new-user-joined', name =>{
+        console.log("New user", name)
         users[socket.id] = name;
-        socket/BroadcastChannel.emit('user-joined', name);
+        socket.broadcast.emit('user-joined', name);
     });
     socket.on('send', message =>{
-        socket.broadcast.emit('recieve', {message: message, name: user[socket.id]})
+        socket.broadcast.emit('recieve', {message: message, name: users[socket.id]})
     });
 })
