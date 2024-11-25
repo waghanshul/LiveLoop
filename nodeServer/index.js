700ea6f694745fb26000c95b3b33cba5ad4643ee
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-11-25 12:34:23 +0530

    major update

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,19 +1,24 @@
 const io = require('socket.io')(8080);
+
 const users = {};
 
 io.on('connection', socket => {
-    console.log(`User connected: ${socket.id}`);
+    // Log when a new user connects
+    console.log("A user connected with ID:", socket.id);
 
+    // Listen for 'new-user-joined' event
     socket.on('new-user-joined', name => {
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
+        console.log("New user joined: " + name);  
+        users[socket.id] = name;  
+        socket.broadcast.emit('user-joined', name); 
     });
 
     socket.on('send', message => {
-        socket.broadcast.emit('receive', { message, name: users[socket.id] });
+        socket.broadcast.emit('recieve', { message: message, name: users[socket.id] });
     });
 
     socket.on('disconnect', () => {
-        delete users[socket.id];
+        console.log("User disconnected:", users[socket.id]);  // Log when a user disconnects
+        delete users[socket.id];  // Clean up the user from the list
     });
 });
