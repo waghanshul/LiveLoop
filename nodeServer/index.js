104431b6d615ea466fd99cdecae1e72573256bfa
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-11-26 12:30:24 +0530

    index update

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -2,42 +2,23 @@ const io = require('socket.io')(8080);
 
 const users = {};
 
-// Constants for event names
-const EVENTS = {
-    CONNECTION: 'connection',
-    NEW_USER_JOINED: 'new-user-joined',
-    SEND: 'send',
-    RECEIVE: 'receive',
-    USER_JOINED: 'user-joined',
-    DISCONNECT: 'disconnect'
-};
+io.on('connection', socket => {
+    // Log when a new user connects
+    console.log("A user connected with ID:", socket.id);
 
-io.on(EVENTS.CONNECTION, (socket) => {
-    console.log(`A user connected with ID: ${socket.id}`);
-
-    // Handle a new user joining
-    socket.on(EVENTS.NEW_USER_JOINED, (name) => {
-        if (name) {
-            console.log(`New user joined: ${name}`);
-            users[socket.id] = name;
-            socket.broadcast.emit(EVENTS.USER_JOINED, name);
-        }
+    // Listen for 'new-user-joined' event
+    socket.on('new-user-joined', name => {
+        console.log("New user joined: " + name);  
+        users[socket.id] = name;  
+        socket.broadcast.emit('user-joined', name); 
     });
 
-    // Handle message sending
-    socket.on(EVENTS.SEND, (message) => {
-        const senderName = users[socket.id];
-        if (message && senderName) {
-            socket.broadcast.emit(EVENTS.RECEIVE, { message, name: senderName });
-        }
+    socket.on('send', message => {
+        socket.broadcast.emit('recieve', { message: message, name: users[socket.id] });
     });
 
-    // Handle user disconnection
-    socket.on(EVENTS.DISCONNECT, () => {
-        const disconnectedUser = users[socket.id];
-        if (disconnectedUser) {
-            console.log(`User disconnected: ${disconnectedUser}`);
-            delete users[socket.id];
-        }
+    socket.on('disconnect', () => {
+        console.log("User disconnected:", users[socket.id]);  // Log when a user disconnects
+        delete users[socket.id];  // Clean up the user from the list
     });
 });
