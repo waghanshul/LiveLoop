84f6bdd9d1d6eef8f658e03ea7b19c682247dddd
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-12-24 12:07:11 +0530

    update

--- js/client.js
+++ js/client.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let userName;
     while (!userName) {
-        userName = prompt("Enter your name join")?.trim();
+        userName = prompt("Enter your name to join")?.trim();
     }
 
     socket.emit('new-user-joined', userName);
