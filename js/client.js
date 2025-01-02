61db77b7358cf1783a61742a112a787852682560
Anshul Wagh <anshulsanjaywagh@gmail.com>
2025-01-02 10:37:53 +0530

    client update

--- js/client.js
+++ js/client.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     socket.emit('new-user-joined', userName);
 
-    socket.on('user-joined', name => appendMessage(`${name}  the chat`, 'center'));
+    socket.on('user-joined', name => appendMessage(`${name} joined the chat`, 'center'));
     socket.on('receive', ({ name, message }) => appendMessage(`${name}: ${message}`, 'left'));
 
     form.addEventListener('submit', (event) => {
